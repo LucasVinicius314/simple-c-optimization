d9bde8b18ad70f8fcf0805df6cde957cf2fbb16c
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2023-03-09 11:14:44 -0300

    Fix

--- src/original.c
+++ src/original.c
@@ -3,7 +3,6 @@
 #include <math.h>
 #define PI 3.141592
 
-/************************************************************************/
 unsigned char ReadElement(FILE *p)
 {
   unsigned char element;
@@ -13,23 +12,26 @@ unsigned char ReadElement(FILE *p)
     exit(-1);
   }
   else
+  {
     return (element);
+  }
 }
-/************************************************************************/
+
 void DetSinCos(unsigned char element, float *sin_element, float *cos_element)
 {
   *sin_element = sin(2.0 * PI * element / 360.0);
   *cos_element = cos(2.0 * PI * element / 360.0);
 }
-/************************************************************************/
+
 void DetOutput(unsigned char element, float *C, float *out_even, float *out_odd)
 {
   float sin_element, cos_element;
+
   DetSinCos(element, &sin_element, &cos_element);
+
   *out_even = 2.0 * sin_element + exp(cos_element);
   *out_odd = exp(sin_element) * C[element];
 }
-/************************************************************************/
 
 int main(int argc, char **argv)
 {
@@ -39,6 +41,8 @@ int main(int argc, char **argv)
   float out_even, out_odd, *Q, C[256];
 
   /***** Comecar a medir o tempo aqui *****/
+
+  int then = (int)time(NULL);
 
   if (argc < 4)
   {
@@ -50,6 +54,7 @@ int main(int argc, char **argv)
     printf("File not found.\n");
     exit(-1);
   }
+
   rows = atoi(argv[2]);
   cols = atoi(argv[3]);
 
@@ -60,8 +65,13 @@ int main(int argc, char **argv)
   }
 
   for (j = 0; j < rows; j++)
+  {
     for (i = 0; i < cols; i++)
+    {
       *(M + j * cols + i) = ReadElement(data_file);
+    }
+  }
+
   fclose(data_file);
 
   if (!(Q = (float *)malloc((long)rows * cols * sizeof(float))))
@@ -71,34 +81,58 @@ int main(int argc, char **argv)
   }
 
   for (i = 0; i < 256; i++)
+  {
     C[i] = 0.0;
+  }
 
   for (i = 0; i < cols; i++)
+  {
     for (j = 0; j < rows; j++)
     {
       element = *(M + j * cols + i);
       C[element]++;
     }
+  }
 
   for (i = 0; i < 256; i++)
+  {
     C[i] = (C[i] > 0) ? log(C[i]) : 0.0;
+  }
 
   for (i = 0; i < cols; i++)
+  {
     for (j = 0; j < rows; j++)
     {
       element = *(M + j * cols + i);
+
       DetOutput(element, C, &out_even, &out_odd);
+
       if ((element % 2) == 0)
+      {
         *(Q + j * cols + i) = pow(out_even, 2);
+      }
       else
+      {
         *(Q + j * cols + i) = out_odd;
+      }
     }
+  }
 
-  /**** Medir o tempo at� aqui *****/
+  int now = (int)time(NULL);
 
-  /**** Sa�da apenas para verifica��o, n�o deve ser medida ****/
+  int diff = now - then;
+
+  printf("%d", diff);
+
+  /**** Medir o tempo até aqui *****/
+
+  /**** Saída apenas para verificação, não deve ser medida ****/
 
   for (i = 0; i < cols; i++)
+  {
     for (j = 0; j < rows; j++)
+    {
       printf("%f ", *(Q + j * cols + i));
+    }
+  }
 }
